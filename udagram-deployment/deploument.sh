df77320cc79c8689781fedc942232726239852ed
teodor98 <tedochakarov@gmail.com>
2022-02-25 14:21:21 +0100

    Update

--- udagram-deployment/deploument.sh
+++ udagram-deployment/deploument.sh
@@ -2,5 +2,4 @@ kubectl apply -f feed-deployment.yaml
 kubectl apply -f user-deployment.yaml
 kubectl apply -f frontend-deployment.yaml
 
-sleep 10
 kubectl apply -f reverseproxy-deployment.yaml
--- udagram-deployment/service.sh
+++ udagram-deployment/service.sh
@@ -2,5 +2,4 @@ kubectl apply -f feed-service.yaml
 kubectl apply -f user-service.yaml
 kubectl apply -f frontend-service.yaml
 
-sleep 10
 kubectl apply -f reverseproxy-service.yaml
